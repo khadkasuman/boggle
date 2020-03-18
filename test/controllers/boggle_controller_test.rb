42565b9cf5353b9b63080d777cb6f1d979bc132c
khadkasuman <khadkasuman20@outlook.com>
2020-03-18 08:31:58 +0545

    regenerate lock file

--- test/controllers/boggle_controller_test.rb
+++ test/controllers/boggle_controller_test.rb
@@ -4,4 +4,8 @@ class BoggleControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  test "should get random sequence of alphabets" do
+    get "boggle/generate"
+    assert !@response.sequence.nil?
+  end
 end
